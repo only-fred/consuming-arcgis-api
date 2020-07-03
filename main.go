adb51b5133552cd87a496729f42608a84edef2cc
only-fred <rochaeduardo997@protonmail.com>
2020-07-03 14:11:06 -0300

    Builded menu and created function to organize data exibition

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type Response struct {
@@ -41,12 +42,77 @@ func main() {
 	var resultObj Response
 	json.Unmarshal(resultData, &resultObj)
 
-	for i := 0; i < len(resultObj.Features); i++ {
-		fmt.Printf("\n> Country: %s\n", resultObj.Features[i].Attributes.CountryRegion)
-		fmt.Printf("> State: %s\n", resultObj.Features[i].Attributes.ProvinceState)
-		fmt.Printf("> Confirmed: %d\n", resultObj.Features[i].Attributes.Confirmed)
-		fmt.Printf("> Recovered: %d\n", resultObj.Features[i].Attributes.Recovered)
-		fmt.Printf("> Deaths: %d\n", resultObj.Features[i].Attributes.Deaths)
-		fmt.Printf("> Active: %d\n", resultObj.Features[i].Attributes.Active)
+	getResultObj(resultObj)
+
+}
+
+func getResultObj(resultObj Response) {
+	answer := "YES"
+	for answer == "YES" {
+		var searchResponse int
+
+		fmt.Println("What do you want to search?")
+		fmt.Printf("[1]ID [2]Country\n->")
+		fmt.Scanf("%d", &searchResponse)
+
+		switch searchResponse {
+		case 1:
+			var searchResponseID int
+
+			fmt.Print("What [ID] you are looking for?\n->")
+			fmt.Scanf("%d", &searchResponseID)
+
+			for i := 0; i < len(resultObj.Features); i++ {
+				if searchResponseID == resultObj.Features[i].Attributes.OBJECTID {
+					getResult(resultObj, i)
+				}
+			}
+
+		case 2:
+			var searchResponseCountry string
+			var searchResponseYesNo string
+			var searchResponseState string
+
+			fmt.Print("What [Country] you are looking for? (ex.: Brazil)\n->")
+			fmt.Scanf("%s", &searchResponseCountry)
+			searchResponseCountry = strings.Title(searchResponseCountry)
+
+			fmt.Print("You are looking for a specific state? (YES/NO)\n->")
+			fmt.Scanf("%s", &searchResponseYesNo)
+			searchResponseYesNo = strings.ToUpper(searchResponseYesNo)
+
+			if searchResponseYesNo == "YES" {
+				fmt.Print("What [State] you are looking for? (ex.: Ceara)\n->")
+				fmt.Scanf("%s", &searchResponseState)
+				searchResponseState = strings.Title(searchResponseState)
+
+				for i := 0; i < len(resultObj.Features); i++ {
+					if searchResponseState == resultObj.Features[i].Attributes.ProvinceState {
+						getResult(resultObj, i)
+					}
+				}
+			} else {
+				for i := 0; i < len(resultObj.Features); i++ {
+					if searchResponseCountry == resultObj.Features[i].Attributes.CountryRegion {
+						getResult(resultObj, i)
+					}
+				}
+			}
+		}
+
+		fmt.Print("Do you want do search again? (YES/NO)\n->")
+		fmt.Scanf("%s", &answer)
+		fmt.Print("\n")
+		answer = strings.ToUpper(answer)
 	}
+}
+
+func getResult(resultObj Response, position int) {
+	fmt.Printf("\n> ID: %d <\n", resultObj.Features[position].Attributes.OBJECTID)
+	fmt.Printf("\n> Country: %s <\n", resultObj.Features[position].Attributes.CountryRegion)
+	fmt.Printf("> State: %s <\n", resultObj.Features[position].Attributes.ProvinceState)
+	fmt.Printf("> Confirmed: %d <\n", resultObj.Features[position].Attributes.Confirmed)
+	fmt.Printf("> Recovered: %d <\n", resultObj.Features[position].Attributes.Recovered)
+	fmt.Printf("> Deaths: %d <\n", resultObj.Features[position].Attributes.Deaths)
+	fmt.Printf("> Active: %d <\n\n", resultObj.Features[position].Attributes.Active)
 }
